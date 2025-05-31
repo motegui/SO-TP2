42fb2b8beaf2158541e243e1ccf226abb815dbcc
matiasrinaldo <mirnaldo@itba.edu.ar>
2025-05-31 15:24:06 -0300

    Started pipes implementation

---
+++ temp/Kernel/include/pipes.h
@@ -0,0 +1,33 @@
+#ifndef PIPES_H
+#define PIPES_H
+
+#include <stdint.h>
+#include "sync.h"
+
+#define PIPE_BUFFER_SIZE 4096
+#define PIPE_NAME_LEN 32
+#define MAX_PIPES 32
+
+
+typedef struct {
+    char name[PIPE_NAME_LEN];
+    char buffer[PIPE_BUFFER_SIZE];
+
+    int read_index;
+    int write_index;
+    int size;
+
+    int open;
+    uint8_t eof;
+
+    Semaphore empty_slots;
+    Semaphore filled_slots;
+} Pipe;
+
+void init_pipes();
+int pipe_open(const char *name);
+int pipe_write(int id, const char *src, int count);
+int pipe_read(int id, char *dest, int count);
+void pipe_close(int id);
+
+#endif
